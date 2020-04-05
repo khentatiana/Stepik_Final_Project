71db3397cfa483caa550af8bce7ceb745f5b6e51
tatiana <khen.tatiana@gmail.com>
2020-04-05 00:48:00 -0700

    test_product_page.py - promo_offer7 is set-up to be xflail

--- Pages/locators.py
+++ Pages/locators.py
@@ -18,9 +18,14 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
-    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner strong")           # This is tag_name(div).class_attribute(alertinner): "div.alertinner"
-    ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")   # This is several class_names separated by " . " (alert-noicon    alert-info  fade  in)
-    PRICE_VALUE = (By.CLASS_NAME, "price_color")                        # This is class_name(price_color)
-    PRODUCT_NAME = (By.TAG_NAME, "h1")                                  # This is tag_name(h1)
-    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")                  # This is tag_name(p).class_attribute(price_color): "p.price_color"
+    # This is tag_name(div).class_attribute(alertinner) space tag_name(strong: "div.alertinner strong"
+    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner strong")
+    # This is several class_names separated by " ." (alert-noicon    alert-info  p)
+    ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")
+    # This is class_name(price_color)
+    PRICE_VALUE = (By.CLASS_NAME, "price_color")
+    # This is tag_name(h1)
+    PRODUCT_NAME = (By.TAG_NAME, "h1")
+    # This is tag_name(p).class_attribute(price_color): "p.price_color"
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
 
--- test_product_page.py
+++ test_product_page.py
@@ -37,7 +37,7 @@ def test_add_to_cart(browser):
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-
+@pytest.mark.xfail(promo_offer_num ="7")
 @pytest.mark.parametrize('promo_offer_num', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
 def test_guest_can_add_product_with_promo_to_basket(browser, promo_offer_num):
     # ваша реализация теста
